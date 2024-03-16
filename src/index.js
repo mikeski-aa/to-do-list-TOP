2d55d1da0acfabcd9d27d9404b310818acad7e9d
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-03-16 12:23:52 +0100

    Create To Do item, create Array for projects
    Created factory function to create new To Do items
    Created a way of storing all todo-items in a specific projct Array
    All to-do items live in their specific project array.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-import './style.css';
+import "./style.css";
 
-console.log('test');
+// factory function to create a new to-do object
+
+const createNewTodo = (title, details, dueDate, prio) => {
+  const toDoTitle = title;
+  const toDoDetails = details;
+  const toDoDueDate = dueDate;
+  const toDoPrio = prio;
+
+  const print = () => {
+    console.log(
+      `Title: ${toDoTitle}, Details: ${toDoDetails}, Due Date: ${toDoDueDate}, Priority: ${toDoPrio}`
+    );
+  };
+
+  return {
+    toDoTitle,
+    toDoDetails,
+    toDoDueDate,
+    toDoPrio,
+    print,
+  };
+};
+
+// storing objects in an array, all to do lists live within their own project
+// additional array manipulation methods need to be added here - i.e removing items from array
+const toDoProject = () => {
+  const array = [];
+
+  function toDoContainer(myItem) {
+    array.push(myItem);
+
+    return {
+      array,
+    };
+  }
+
+
+  return {
+    toDoContainer,
+    array,
+  };
+};
+
+
+// console testing stuff
+let myItem = createNewTodo("Wash", "Was car", "today", "medium");
+let myItem2 = createNewTodo("Clean", "Clean room", "tomorrow", "high");
+myItem.print();
+myItem2.print();
+
+let container = toDoProject();
+container.toDoContainer(myItem);
+container.toDoContainer(myItem2);
+console.log(container.array);
